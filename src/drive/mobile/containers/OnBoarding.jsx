62619aa49a37a2a33091ec1091fb6b55b69d5e34
Yannick Lohse <contact@yannick-lohse.fr>
2017-08-17 15:38:49 +0200

    refactor: allow different onboarding steps

--- src/drive/mobile/containers/OnBoarding.jsx
+++ src/drive/mobile/containers/OnBoarding.jsx
@@ -1,25 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 
+
+import ServerSelectionWizard from './ServerSelectionWizard'
 import Wizard from '../components/Wizard'
-import Welcome from './onboarding/Welcome'
-import SelectServer from './onboarding/SelectServer'
+
 import Files from './onboarding/Files'
 import BackupPhotosVideos from './onboarding/BackupPhotosVideos'
 import BackupContacts from './onboarding/BackupContacts'
 import Analytics from './onboarding/Analytics'
 
-const OnBoarding = (props) => {
-  const steps = [
-    Welcome,
-    SelectServer
-  ]
-  const stepsWithBreadcrumb = [
-    Files,
-    BackupPhotosVideos,
-    BackupContacts,
-    Analytics
-  ]
-  return <Wizard steps={steps} breadcrumbSteps={stepsWithBreadcrumb} {...props} />
+export default class OnBoarding extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      hasSelectedServer: false
+    }
+
+    this.onboardingSteps = [
+      Files,
+      BackupPhotosVideos,
+      BackupContacts,
+      Analytics
+    ]
+  }
+
+  afterServerSelection () {
+    this.setState({ hasSelectedServer: true })
+  }
+
+  afterWizard () {
+    if (this.props.location.state && this.props.location.state.nextPathname) {
+      this.props.router.replace(this.props.location.state.nextPathname)
+    } else {
+      this.props.router.replace('/')
+    }
+  }
+
+  render () {
+    const { hasSelectedServer } = this.state
+
+    return hasSelectedServer === false
+      ? <ServerSelectionWizard onComplete={() => this.afterServerSelection()} />
+      : <Wizard steps={this.onboardingSteps} onComplete={() => this.afterWizard()} />
+  }
 }
 
-export default OnBoarding
